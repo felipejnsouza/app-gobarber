a872baa9e339f7482bc8d4e20c56d68b4ff0c531
Felipe Souza <fnasci@gmail.com>
2021-04-01 20:25:29 -0300

    validating forms

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,12 @@
-import React, { useRef } from 'react';
-import { Image, ScrollView, TextInput } from 'react-native';
+import React, { useRef, useCallback } from 'react';
+import { Alert, Image, ScrollView, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -18,7 +21,46 @@ import {
 
 import logoImg from '../../assets/logo.png';
 
+interface SignUpFormData {
+    name: string;
+    email: string;
+    password: string;
+}
+
 const SignUp: React.FC = () => {
+    const handleSignUp = useCallback(async (data: SignUpFormData) => {
+        try {
+            formRef.current?.setErrors({});
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string()
+                    .required('E-mail obrigatório')
+                    .email('Digite um e-mail válido'),
+                password: Yup.string()
+                    .min(6, 'No mínimo 6 dígitos')
+            });
+
+            await schema.validate(data, {
+                abortEarly: false
+            });
+
+            // await api.post('/users', data);
+
+            // history.push('/');
+
+        } catch (error) {
+            if (error instanceof Yup.ValidationError) {
+                const errors = getValidationErrors(error);
+
+                formRef.current?.setErrors(errors)
+
+                return;
+            }
+
+            Alert.alert('Erro no cadastro', 'Ocorreu um erro ao fazer o cadastro, tente novamente.');
+        }
+    }, [])
+
     const emailInputRef = useRef<TextInput>(null);
     const passwordInputRef = useRef<TextInput>(null);
 
@@ -36,7 +78,7 @@ const SignUp: React.FC = () => {
                     <Image source={logoImg} />
                     <Title>Crie sua conta</Title>
 
-                    <Form ref={formRef} onSubmit={(data) => console.log(data)}>
+                    <Form ref={formRef} onSubmit={handleSignUp}>
                         <Input
                             autoCapitalize="words"
                             name="name"
