3df98d4a75051e6f6304b2eb4f1ebf99531c020b
Felipe Souza <fnasci@gmail.com>
2021-03-31 21:25:35 -0300

    register screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -17,6 +18,8 @@ import {
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
+    const navigation = useNavigation();
+
     return (
         <>
             <ScrollView
@@ -38,7 +41,7 @@ const SignIn: React.FC = () => {
                 </Container>
             </ScrollView>
 
-            <CreateAccountButton onPress={() => { 'criar' }} >
+            <CreateAccountButton onPress={() => navigation.navigate('SignUp')} >
                 <Icon name="log-in" size={20} color="#ff9000" />
                 <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
             </CreateAccountButton>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { Image, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+    Container,
+    Title,
+    BackToSignIn,
+    BackToSignInText
+} from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignUp: React.FC = () => {
+    const navigation = useNavigation();
+
     return (
-        <Container />
+        <>
+            <ScrollView
+                keyboardShouldPersistTaps="handled"
+                contentContainerStyle={{ flex: 1 }}
+            >
+                <Container>
+                    <Image source={logoImg} />
+                    <Title>Crie sua conta</Title>
+
+                    <Input name="name" icon="user" placeholder="Nome" />
+                    <Input name="email" icon="mail" placeholder="E-mail" />
+                    <Input name="password" icon="lock" placeholder="Senha" />
+
+                    <Button onPress={() => console.log("clicou")}>Entrar</Button>
+
+                </Container>
+            </ScrollView>
+
+            <BackToSignIn onPress={() => navigation.goBack()} >
+                <Icon name="arrow-left" size={20} color="#fff" />
+                <BackToSignInText>Voltar para logon</BackToSignInText>
+            </BackToSignIn>
+        </>
     );
 };
 
